04d10991fd2d9bc4fc9cad944e7f9cddc8944bd4
rot <marco.rotondi@intre.it>
2025-10-28 23:24:53 +0100

    feat(order-service): Validate pizzas exist before creating order

--- src/main/java/com/marco/awesomepizza/pizzachef/PizzaChefWorker.java
+++ src/main/java/com/marco/awesomepizza/pizzachef/PizzaChefWorker.java
@@ -1,10 +1,13 @@
 package com.marco.awesomepizza.pizzachef;
 
+import com.marco.awesomepizza.order.entity.OrderEntity;
 import com.marco.awesomepizza.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 @Slf4j
@@ -20,12 +23,30 @@ public class PizzaChefWorker {
     public void doPizza() {
         orderService.getOrderToProcessing()
                 .switchIfEmpty(Mono.defer(() -> Mono.error(new OrderNotFoundException("No order to processing!"))))
-                .mapNotNull(orderEntity -> {
-                    log.info("Processing order {}", orderEntity.getId());
-                    return null;
-                })
+                .mapNotNull(this::preparePizza)
+                .delayElement(Duration.ofMinutes(1))
+                .mapNotNull(this::cooking)
+                .delayElement(Duration.ofMinutes(1))
                 .onErrorComplete(this::logOrderProcessingError)
+                .doOnNext(this::completeOrder)
                 .subscribe();
+    }
+
+    private OrderEntity preparePizza(OrderEntity orderEntity) {
+        log.info("Preparing order {}", orderEntity.getId());
+
+
+        return null;
+    }
+
+    private OrderEntity cooking(OrderEntity orderEntity) {
+        log.info("Cooking order {}", orderEntity.getId());
+
+        return null;
+    }
+
+    private void completeOrder(OrderEntity orderEntity) {
+        log.info("Order {} completed", orderEntity.getId());
     }
 
     private boolean logOrderProcessingError(Throwable error) {
